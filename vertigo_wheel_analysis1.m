2e4c3b7b7ba406ede02989574f841e066a2e26e8
Luke Gonsalves <darkluke210@hotmail.co.uk>
2017-06-24 09:50:57 +0100

    First Pull of Wheel Analysis Scripts

---
+++ vertigo_wheel_analysis1.m
@@ -0,0 +1,69 @@
+% Jon Sowman 2017
+% Jamie Costello
+% jon+vertigo@jonsowman.com
+%
+% This file is part of the Vertigo project
+%
+% Rotate the IMU data into the NED frame using the AHRS data
+
+accel_ned = zeros(length(imudata), 3);
+gyro_ned  = zeros(length(imudata), 3);
+
+for i = 1:length(imudata)
+    quat_int = interp1(quatdata(:,1), quatdata(:, 3:6), imudata(i,1));
+    a = [0 imudata(i, 3:5)];
+    g = [0 imudata(i, 6:8)];
+    aa = quatmultiply(quatmultiply(quat_int, a), quatconj(quat_int));
+    accel_ned(i, :) = aa(2:4);
+    gg= quatmultiply(quatmultiply(quat_int, g), quatconj(quat_int));
+    gyro_ned(i, :) = gg(2:4);
+end
+
+subplot (3,1,1);
+time = [diff(imudata(:,1)); 0.01];
+North = cumtrapz(imudata(:,1), 9.81 * accel_ned(:,1));
+North = cumtrapz(imudata(:,1), North);
+East = cumtrapz(imudata(:,1), 9.81 * accel_ned(:,2));
+East = cumtrapz(imudata(:,1), East);
+Down = cumtrapz(imudata(:,1), 9.81 * (accel_ned(:,3) - 0.981));
+Down = cumtrapz(imudata(:,1), Down);
+%plot raw positions
+plot (imudata(:,1), North, imudata(:,1), East,imudata(:,1), Down);
+
+xlabel('Time (s)');
+ylabel('NED Position(m) ')
+
+
+%find polyfit coefficient Values for each of North, East Down (PCN, PCE,
+%PCD) with polynomial of order 10- which may well be excessive.
+% Create a polyfit values (PVN, PVE, PVD) for all times (imudata (:,1) -all times from
+% column 1)
+pcn = polyfit(imudata(:,1),North,10)
+pvn = polyval(pcn,imudata(:,1));
+pce = polyfit(imudata(:,1),East,10)
+pve = polyval(pce,imudata(:,1));
+pcd = polyfit(imudata(:,1),Down(:),4)
+pvd = polyval(pcd,imudata(:,1));
+subplot (3,1,2);
+
+plot (imudata(:,1), North - pvn)
+%, imudata(:,1), East- pve,imudata(:,1), Down(:)- pvd
+xlabel('Time (s)');
+ylabel('NED Position(m) ')
+
+
+%choose times to look at in detail - I think between 5 and 20 seconds so:
+
+Corrected_North = North (500:2000)- pvn(500:2000);
+Corrected_East = East (500:2000)-pve(500:2000);
+Corrected_Down = Down (500:2000)-pvd(500:2000);
+Time = imudata(500:2000)- imudata(500);
+subplot (3,1,3);
+plot ( Time, Corrected_North);
+%Time, Corrected_North, Time, Corrected_East,
+%Not bad.... but there's more to do in vertigo_wheel_analysis2
+xlabel('Time (s)');
+ylabel('N Position(m) ')
+
+%figure
+%plot (Corrected_North, Corrected_East)
---
+++ vertigo_wheel_analysis2.m
@@ -0,0 +1,42 @@
+plot (Time, Corrected_North);
+% once plotted pick the cursor data tool on the figure screen.  Mark a
+% point at the top of the first blue peak.  Then right click, add new
+% cursor data point and point out the second blue peak.  You may have to
+% move the information square.  Do this for all of the blue peaks- IN
+% ORDER!
+
+%finally, right click and export data to workspace
+%Create a matrix for this new data call it A
+
+A = [cursor_info.Position];
+
+
+%All of my times are X coordintaes and they area in the odd positions in the columns so:
+%My new matrix T holds all of the time values 
+T = A([1 3 5 7 9 11 13 15 17]);
+
+%Similarly my North positions are all in even slots so:
+N = A ([2 4 6 8 10 12 14 16 18])
+
+figure
+%plot (T, N);
+% now find a polyfit coefficients for this new graph
+pcN = polyfit(T,N,5);
+
+%find values in Time - remember time is 5- 20 seconds from the imudata
+pvN = polyval(pcN,Time);
+
+plot (Time, pvN);
+% and finally we plot the most corrected graph - not pvN' that means I have
+% swapped Columns into rows- that's all.
+plot (Time, (Corrected_North - pvN'));
+%plot (Time, (Corrected_East - pvE'));
+% you need to go through this process for the East direction.
+% To check progress you can always remove the % sign and plot one of the
+% graphs - play around with the power of the polyfit curve to get a better
+% fit.
+
+
+
+
+
