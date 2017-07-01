735009109a3f23cf3423fba3711f43ce77f643da
Xenocyte <dpr587.dr@gmail.com>
2017-07-01 20:18:33 +0100

    Add files via upload

--- matlab/vtg_load_data_original.m
+++ matlab/vtg_load_data_original.m
@@ -3,6 +3,7 @@
 % Jon Sowman 2017
 % Tom Ronayne 2017
 % Luke Gonsalves 2017
+% David Rudolph 2017
 % jon+vertigo@jonsowman.com
 
 % Ignoring gps for now (btw imu is acceleration data)
@@ -40,7 +41,7 @@ imudatasplit = permute(imudata(:,3:5),[2 3 1]); %instead of n by m its 1 by m by
 
 aworld = zeros(3,length(quatdata));
 for i = 1:length(quatdata)
-aworld(:,i) = rotm(:,:,i) * imudatasplit(1:3,:,i); %rotation matrix applied to each data point
+    aworld(:,i) = rotm(:,:,i) * imudatasplit(1:3,:,i); %rotation matrix applied to each data point
 end
 aworld = aworld'; %flips it the right way up (wide to tall)
 
@@ -56,94 +57,96 @@ apolyx = polyfit(quatdata(int,1),ax(int),1);
 apolyy = polyfit(quatdata(int,1),ay(int),1);
 apolyz = polyfit(quatdata(int,1),az(int),1);
 for i = 1:length(quatdata)
-axc(i,1) = smoothax(i) - apolyx(1)*quatdata(i,1) - apolyx(2);
-ayc(i,1) = smoothay(i) - apolyy(1)*quatdata(i,1) - apolyy(2);
-azc(i,1) = smoothaz(i) - apolyz(1)*quatdata(i,1) - apolyz(2);
+    axc(i,1) = smoothax(i) - apolyx(1)*quatdata(i,1) - apolyx(2);
+    ayc(i,1) = smoothay(i) - apolyy(1)*quatdata(i,1) - apolyy(2);
+    azc(i,1) = smoothaz(i) - apolyz(1)*quatdata(i,1) - apolyz(2);
 end
 for i = 1:600 %remove acceleration at the begining to remove velocity drift
-axc(i) = 0;
-ayc(i) = 0;
-azc(i) = 0;
+    axc(i) = 0;
+    ayc(i) = 0;
+    azc(i) = 0;
 end
-vx = cumsum(axc);
-vy = cumsum(ayc);
-vz = cumsum(azc);
-vpolyx = polyfit(quatdata(int,1),vx(int),2);
-vpolyy = polyfit(quatdata(int,1),vy(int),2);
-vpolyz = polyfit(quatdata(int,1),vz(int),2);
+    vx = cumsum(axc);
+    vy = cumsum(ayc);
+    vz = cumsum(azc);
+    vpolyx = polyfit(quatdata(int,1),vx(int),2);
+    vpolyy = polyfit(quatdata(int,1),vy(int),2);
+    vpolyz = polyfit(quatdata(int,1),vz(int),2);
 for i = 1:length(quatdata)
-vxc(i,1) = vx(i) - vpolyx(1)*(quatdata(i,1))^2 - vpolyx(2)*quatdata(i,1) - vpolyx(3);
-vyc(i,1) = vy(i) - vpolyy(1)*(quatdata(i,1))^2 - vpolyy(2)*quatdata(i,1) - vpolyy(3);
-vzc(i,1) = vz(i) - vpolyz(1)*(quatdata(i,1))^2 - vpolyz(2)*quatdata(i,1) - vpolyz(3);
+    vxc(i,1) = vx(i) - vpolyx(1)*(quatdata(i,1))^2 - vpolyx(2)*quatdata(i,1) - vpolyx(3);
+    vyc(i,1) = vy(i) - vpolyy(1)*(quatdata(i,1))^2 - vpolyy(2)*quatdata(i,1) - vpolyy(3);
+    vzc(i,1) = vz(i) - vpolyz(1)*(quatdata(i,1))^2 - vpolyz(2)*quatdata(i,1) - vpolyz(3);
 end
 for i = 1:600 %remove velocity
-vxc(i) = 0;
-vyc(i) = 0;
-vzc(i) = 0;
+    vxc(i) = 0;
+    vyc(i) = 0;
+    vzc(i) = 0;
 end
-x = cumsum(vxc);
-y = cumsum(vyc);
-z = cumsum(vzc);
-t = imudata(1:length(quatdata),1);
+    x = cumsum(vxc);
+    y = cumsum(vyc);
+    z = cumsum(vzc)
+    t = imudata(1:length(quatdata),1);
 
 % Plot raw imu data
 % Accelerations
-subplot(3,2,1);
-hold on;
-plot(t, axc);
-plot(t, ayc);
-plot(t, azc);
-xlabel('Time (s)');
-ylabel('Acceleration (g)');
-legend('x', 'y', 'z');
+    subplot(3,2,1);
+    hold on;
+    plot(t, axc);
+    plot(t, ayc);
+    plot(t, azc);
+    xlabel('Time (s)');
+    ylabel('Acceleration (g)');
+    legend('x', 'y', 'z');
+
+% Plot Accelerations Against Time
 
 % Velocity TOM MADE TIHS
-subplot(3,2,2);
-hold on;
-plot(t, vxc);
-plot(t, vyc);
-plot(t, vzc);
-xlabel('Time (s)');
-ylabel('Velocity (idek)');
-legend('x', 'y', 'z');
+    subplot(3,2,2);
+    hold on;
+    plot(t, vxc);
+    plot(t, vyc);
+    plot(t, vzc);
+    xlabel('Time (s)');
+    ylabel('Velocity (idek)');
+    legend('x', 'y', 'z');
 
-subplot(3,2,3);
-hold on;
-plot(t, x);
-plot(t, y);
-plot(t, z);
-xlabel('Time (s)');
-ylabel('Displacement (idek)');
-legend('x', 'y', 'z');
+    subplot(3,2,3);
+    hold on;
+    plot(t, x);
+    plot(t, y);
+    plot(t, z);
+    xlabel('Time (s)');
+    ylabel('Displacement (idek)');
+    legend('x', 'y', 'z');
 
 % Rate gyros
 % Experimental Filter
-gyrox = smooth(imudata(:,6), 0.3, 'sgolay');
-gyroy = smooth(imudata(:,7), 0.3, 'sgolay');
-gyroz = smooth(imudata(:,8), 0.3, 'sgolay');
-subplot(3,2,4);
-hold on;
-plot(imudata(:,1), imudata(:,6:8), '-');
-hold on;
-plot(imudata(:,1), gyrox);
-plot(imudata(:,1), gyroy);
-plot(imudata(:,1), gyroz);
-xlabel('Time (s)');
-ylabel('Gyro (deg/s)');
-legend('x', 'y', 'z');
+    gyrox = smooth(imudata(:,6), 0.3, 'sgolay');
+    gyroy = smooth(imudata(:,7), 0.3, 'sgolay');
+    gyroz = smooth(imudata(:,8), 0.3, 'sgolay');
+    subplot(3,2,4);
+    hold on;
+    plot(imudata(:,1), imudata(:,6:8), '-');
+    hold on;
+    plot(imudata(:,1), gyrox);
+    plot(imudata(:,1), gyroy);
+    plot(imudata(:,1), gyroz);
+    xlabel('Time (s)');
+    ylabel('Gyro (Rad/s)');
+    legend('x', 'y', 'z');
 
 % Plot DMP data
-subplot(3,2,5);
-plot(quatdata(:,1), euldata);
-xlabel('Time (s)');
-ylabel('Orientation (deg)');
-legend('roll', 'pitch', 'yaw');
+    subplot(3,2,5);
+    plot(quatdata(:,1), euldata);
+    xlabel('Time (s)');
+    ylabel('Orientation (deg)');
+    legend('roll', 'pitch', 'yaw');
 
 %animated plot
-figure
-plot3(x, y, z);
-xlabel('X displacement');
-ylabel('Y displacement');
-zlabel('Z displacement');
+    figure
+    plot3(x, y, z);
+    xlabel('X displacement');
+    ylabel('Y displacement');
+    zlabel('Z displacement');
 
 
