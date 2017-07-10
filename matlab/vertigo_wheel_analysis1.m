2e10e42aeb60be070549fc5cce09b078f337619d
Luke Gonsalves <darkluke210@hotmail.co.uk>
2017-07-10 14:52:22 +0100

    Updated
    RUN raw data, wheel analysis 1, proc_rocket

--- matlab/vertigo_wheel_analysis1.m
+++ matlab/vertigo_wheel_analysis1.m
@@ -7,7 +7,7 @@
 % Rotate the IMU data into the NED frame using the AHRS data
 accel_ned = zeros(length(imudata), 3);
 gyro_ned  = zeros(length(imudata), 3);
-
+t = imudata(:,1);
 for i = 1:length(imudata)
     quat_int = interp1(quatdata(:,1), quatdata(:, 3:6), imudata(i,1));
     a = [0 imudata(i, 3:5)];
@@ -27,28 +27,27 @@ East = cumtrapz(imudata(:,1), East);
 Down = cumtrapz(imudata(:,1), 9.81 * (accel_ned(:,3) - 0.981));
 Down = cumtrapz(imudata(:,1), Down);
 %plot raw positions
-plot (imudata(:,1), North, imudata(:,1), East,imudata(:,1), Down);
+plot (t, North, t, East, t, Down);
 
 xlabel('Time (s)');
-ylabel('NED Position(m) ')
+ylabel('NED Position(m) ');
 
 
 %find polyfit coefficient Values for each of North, East Down (PCN, PCE,
 %PCD) with polynomial of order 10- which may well be excessive.
 % Create a polyfit values (PVN, PVE, PVD) for all times (imudata (:,1) -all times from
 % column 1)
-pcn = polyfit(imudata(:,1),North,10)
+pcn = polyfit(imudata(:,1),North,1);
 pvn = polyval(pcn,imudata(:,1));
-pce = polyfit(imudata(:,1),East,10)
+pce = polyfit(imudata(:,1),East,1);
 pve = polyval(pce,imudata(:,1));
-pcd = polyfit(imudata(:,1),Down(:),4)
+pcd = polyfit(imudata(:,1),Down(:),1);
 pvd = polyval(pcd,imudata(:,1));
 subplot (3,1,2);
 
-plot (imudata(:,1), North - pvn)
-%, imudata(:,1), East- pve,imudata(:,1), Down(:)- pvd
+plot (t, North - pvn, t, East - pve,t , Down(:)- pvd);
 xlabel('Time (s)');
-ylabel('NED Position(m) ')
+ylabel('NED Position(m) ');
 
 
 %choose times to look at in detail - I think between 5 and 20 seconds so:
@@ -62,7 +61,7 @@ plot ( Time, Corrected_North);
 %Time, Corrected_North, Time, Corrected_East,
 %Not bad.... but there's more to do in vertigo_wheel_analysis2
 xlabel('Time (s)');
-ylabel('N Position(m) ')
+ylabel('N Position(m) ');
 
 %figure
 %plot (Corrected_North, Corrected_East)
