c9e94d1648c5eea1e73e787b1d9e40ffa50a93e0
Ben Oxley <ben.oxley@gmail.com>
2018-07-01 14:51:22 +0100

    Progress with file selector and parser

---
+++ python/tools/FileParser.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import FileSelector
+
+class FileParser:
+
+    @staticmethod
+    def selectAndOpen():
+        file_path = FileSelector.select()
+        data = FileParser.parse(file_path)
+        data = FileParser.fixRolloverError(data)
+        gpsData = FileParser.getDataType(data,1)
+        imuData = FileParser.getDataType(data,2)
+        quatData = FileParser.getDataType(data,3)
+        return [gpsData,imuData,quatData]
+
+    @staticmethod
+    def parse(file_path):
+        data = pd.read_csv(file_path)
+        return data
+
+    @staticmethod
+    def getDataType(dataFrame, type_interger_identifier):
+        return dataFrame.loc[dataFrame[1] == type_interger_identifier]
+
+    @staticmethod
+    def fixRolloverError(dataFrame):
+        rollover_value = 2^32/1e4
+        delta_times = dataFrame.iloc[:,[0]].diff()
+        negative_delta_times = delta_times.loc[delta_times[0] < 0]
+        for row in negative_delta_times:
+            dataFrame = dataFrame[:,row.index():-1] + rollover_value
+        return dataFrame
+            
+
+
+
--- python/tools/FileSelector.py
+++ python/tools/FileSelector.py
@@ -1,15 +1,13 @@
+import pandas
 import tkinter as tk
 from tkinter import filedialog
 
-class File-Selector:
+
+class FileSelector:
 
     @staticmethod
     def select():
         root = tk.Tk()
         root.withdraw()
-        file_path = filedialog.askopenfilename()
+        file_path = filedialog.askopenfilename(filetypes = (("Vertigo Datalogger files", "*.csv"),("All files", "*.*") ))
         return file_path
-
-    @staticmethod
-    def selectAndOpen():
-        file_path = select()
